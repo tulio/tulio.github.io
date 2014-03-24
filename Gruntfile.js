dd7f8c599f72eaf9895c3549e392ef22faa4cac6
Túlio Freitas <tuliofreitas@live.com>
2014-03-24 01:01:27 -0300

    Incluindo arquivos de configuração do Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function(grunt){
+	// Set up the configuraton object.
+	var config = {};
+
+	// All tasks that must be loaded.
+	var tasks = [
+			"grunt-contrib-jshint"
+		, "grunt-contrib-watch"
+		, "grunt-contrib-concat"
+		, "grunt-contrib-uglify"
+		, "grunt-contrib-sass"
+	];
+
+	// Concat ===========================================================
+	var concat;
+	config.concat = concat = {};
+
+	concat.dev = {
+		files: {
+			"assets/javascripts/myapp.development.js": [
+					"assets/javascripts/jquery.js"
+				,	"assets/javascripts/tipuesearch.js"
+				, "assets/javascripts/tipuesearch_set.js"
+				, "assets/javascripts/scripts.js"
+			]
+		}
+	};
+
+
+	// JShint ===========================================================
+	var jshint;
+	config.jshint = jshint = {};
+
+	jshint.dist = {
+		options: {
+				strict: true
+			, laxcomma: true
+			, sub: true
+		},
+		files: {all: ["assets/javascripts/scripts.js"]}
+	};
+
+	jshint.dev = {
+		options: {
+				strict: true
+			, laxcomma: true
+			, sub: true
+			, debug: true
+		},
+		files: {all: ["assets/javascripts/scripts.js"]}
+	};
+
+	// Uglify ===========================================================
+	config.uglify = {dist: {
+		options: {
+			sourceMap: true,
+      sourceMapName: "assets/javascripts/myapp.map"
+		},
+		files: {
+			"assets/javascripts/myapp.production.js": ["assets/javascripts/myapp.development.js"]
+		}
+	}}
+
+	// Sass =============================================================
+	var sass;
+	config.sass = sass = {};
+
+	sass.dist = {
+			options: {style: "compressed"}
+		, files: {
+			"assets/stylesheets/style.css": "scss/style.scss"
+		}
+	};
+
+	sass.dev = {
+			options: {style: "expanded", lineNumber: true}
+		, files: {
+			"assets/stylesheets/style.development.css": "scss/style.scss"
+		}
+	};
+
+
+	// Watch ============================================================
+	config.watch = {
+		scripts: {
+				files: ["assets/javascripts/scripts.js", "scss/**/*.scss"]
+			, tasks: ["dev"]
+		}
+	};
+
+	// Register custom tasks ============================================
+	grunt.registerTask("default", ["dev"]);
+	grunt.registerTask("dev", ["jshint:dev", "concat:dev", "sass:dev"]);
+	grunt.registerTask("dist", ["jshint:dist", "concat:dev", "uglify", "sass"]);
+
+	// General setup ====================================================
+	grunt.initConfig(config);
+	tasks.forEach(grunt.loadNpmTasks);
+};
