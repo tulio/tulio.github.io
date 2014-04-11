033ea839a72ac2060a12467c443a6822137dfb50
Túlio Freitas <tuliofreitas@live.com>
2014-04-10 23:54:08 -0300

    Novos scripts, agora com configuração de resultados de pesquisas.

--- assets/javascripts/myapp.dev.js
+++ assets/javascripts/myapp.dev.js
@@ -10351,7 +10351,7 @@ http://www.tipue.com/search
 
           var set = $.extend( {
           
-               'show'                   : 2,
+               'show'                   : 10,
                'newWindow'              : false,
                'showURL'                : true,
                'minimumLength'          : 1,
@@ -10570,7 +10570,7 @@ http://www.tipue.com/search
                               else
                               {
                                    c_c = c.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                                   out += '<div id="tipue_search_results_count">' + c_c + ' Resultados</div>';
+                                   out += '<div class="count" id="tipue_search_results_count">' + c_c + ' Resultados</div>';
                               }
                               
                               found.sort();
